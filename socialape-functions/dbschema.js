267b35aef78859c169a9caa9e237abc5fe7190f3
Kyle Hallman <khallman@my.yorku.ca>
2020-03-21 19:15:57 -0400

    First

---
+++ socialape-functions/dbschema.js
@@ -0,0 +1,23 @@
+let db = {
+    users: [
+        {
+            userId:'gfgdfgdfgdqd',
+            email:'user@email.com',
+            handle:'user',
+            createdAt: "sometime",
+            imageUrl: 'someurl',
+            bio: 'user bio',
+            website: 'user web',
+            location: 'some location'
+        }
+    ],
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'This is the scream body',
+            createdAt: '2019-03-15T11:46:01:018Z',
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
---
+++ socialape-functions/functions/util/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apiKey: "AIzaSyCSXXA7bWQcWvNpb1vIF0ui-XBcuKk4UjE",
+    authDomain: "socialape-65d23.firebaseapp.com",
+    databaseURL: "https://socialape-65d23.firebaseio.com",
+    projectId: "socialape-65d23",
+    storageBucket: "socialape-65d23.appspot.com",
+    messagingSenderId: "830773210444",
+    appId: "1:830773210444:web:30ccd0f373f36b596fb3e9",
+    measurementId: "G-RVE6PL9P5K"
+};
